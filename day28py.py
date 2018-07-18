f6b98ad7a2c38875c2b3c725207138917d1e5916
vishalprabha <vishal.prabhachandar1@gmail.com>
2018-07-18 16:53:43 +0530

    Day-28

---
+++ day28py.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    list = []
+    for N_itr in range(N):
+        firstNameEmailID = input().split()
+
+        firstName = firstNameEmailID[0]
+
+        emailID = firstNameEmailID[1]
+        if re.search('@gmail\.com$',emailID):
+            list.append(firstName)
+    list.sort()
+    print("\n".join(list))
